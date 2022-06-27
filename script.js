d04234e0a3c606e0666df316d224952376583130
Anushavan <hwifi097@gmail.com>
2022-06-27 21:17:41 +0500

    Начинаем писать приложение №-1

---
+++ script.js
@@ -0,0 +1,20 @@
+//Первое задание 
+const numberOfFilms = + prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("Насколько оцениваете его?", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцениваете его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
